086050821cf2cd6532396013f2a36902fb9ca34f
daz-3ux <daz-3ux@proton.me>
2023-10-28 19:06:52 +0800

    test: add test for LRU
    Signed-off-by: daz-3ux <daz-3ux@proton.me>

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,7 @@
 package lru
 
 import (
+    "reflect"
     "testing"
 )
 
@@ -36,5 +37,19 @@ func TestCache_RemoveOldest(t *testing.T) {
 }
 
 func TestCache_TestOnEvicted(t *testing.T) {
+    keys := make([]string, 0)
+    callback := func(key string, value Value) {
+        keys = append(keys, key)
+    }
+    lru := New(int64(10), callback)
+    lru.Add("key1", String("123456"))
+    lru.Add("k2", String("v2"))
+    lru.Add("k3", String("v3"))
+    lru.Add("k4", String("v4"))
 
+    expect := []string{"key1", "k2"}
+
+    if !reflect.DeepEqual(expect, keys) {
+        t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+    }
 }
