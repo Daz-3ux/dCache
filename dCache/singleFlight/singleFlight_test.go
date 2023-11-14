5342e056227f4250796880df7c928b0e9b420ee5
daz-3ux <daz-3ux@proton.me>
2023-11-14 21:55:05 +0800

    feat: support Update func
    Signed-off-by: daz-3ux <daz-3ux@proton.me>

---
+++ dCache/singleFlight/singleFlight_test.go
@@ -0,0 +1,34 @@
+package singleFlight
+
+import (
+    "errors"
+    "fmt"
+    "testing"
+)
+
+func TestDo(t *testing.T) {
+    var g Group
+    v, err := g.Do("key", func() (interface{}, error) {
+        return "bar", nil
+    })
+    if got, want := fmt.Sprintf("%v (%T)", v, v), "bar (string)"; got != want {
+        t.Errorf("Do = %v; want %v", got, want)
+    }
+    if err != nil {
+        t.Errorf("Do error = %v", err)
+    }
+}
+
+func TestDoErr(t *testing.T) {
+    var g Group
+    someErr := errors.New("some error")
+    v, err := g.Do("key", func() (interface{}, error) {
+        return nil, someErr
+    })
+    if !errors.Is(err, someErr) {
+        t.Errorf("Do error = %v; want someErr", err)
+    }
+    if v != nil {
+        t.Errorf("unexpected non-nil value %#v", v)
+    }
+}
